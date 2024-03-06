52e694b3c65ea86827a27f5fe41859868a439548
zhangzhaoyu.123 <zhangzhaoyu.123@bytedance.com>
2024-03-06 19:53:51 +0800

    feat: simplize

--- src/admin/admin.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get, HostParam } from '@nestjs/common';
-
-@Controller({ host: ':account.example.com', path: 'account' })
-export class AdminController {
-  @Get()
-  getInfo(@HostParam('account') account: string) {
-    return account;
-  }
-}
